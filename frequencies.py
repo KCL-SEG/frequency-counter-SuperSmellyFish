380a1fafe4eb82c12a7fe0a850f07ae9145861db
Kai <kingarthur.jones64@gmail.com>
2023-10-06 23:57:37 +0100

    third cleaned up version of frequencies function

--- frequencies.py
+++ frequencies.py
@@ -22,31 +22,14 @@
 #     return frequencies
 
 def frequencies(items):
-    # Initialize an empty dictionary to store the frequencies.
     freq_dict = {}
-    
-    # Iterate through the items list.
+
     for item in items:
-        # Convert the item to a string to use it as a key.
         key = str(item)
         
-        # Check if the key is already in the dictionary.
         if key in freq_dict:
-            # If it's in the dictionary, increment the count.
             freq_dict[key] += 1
         else:
-            # If it's not in the dictionary, add it with a count of 1.
             freq_dict[key] = 1
     
     return freq_dict
-
-# Test cases
-print(frequencies(['a', 'a', 'b', 'b', 'b', 'c']))  # Output: {'a': 2, 'b': 3, 'c': 1}
-print(frequencies([100, 'Hello', '100', '100', 100]))  # Output: {'100': 4, 'Hello': 1}
-
-
-input = [100, 'Hello', '100', '100', 100]
-
-output = frequencies(input)
-
-print(output)
