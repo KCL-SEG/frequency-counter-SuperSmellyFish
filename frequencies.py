89e2952a366146ad220d4d5bad58edb894a8d492
Kai <kingarthur.jones64@gmail.com>
2023-10-06 21:26:25 +0100

    working frequencies function 1

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,22 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
-
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for i in range(0,len(items)):
+        if isinstance(items[i], str):
+            pass
+        else:
+            items[i] = ''+str(items[i])+''
+    
+    items.sort()
+    count = 1
+
+    for i, el in enumerate(items[:-1]):
+        if items[i] == items[i + 1]:
+            count += 1
+        else:
+            frequencies[items[i]] = count
+            count = 1
+
     return frequencies
